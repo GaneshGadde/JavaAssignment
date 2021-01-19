dc6a5496c12b763a2383b9d485fd43e3e5874935
GaneshGadde <77649907+GaneshGadde@users.noreply.github.com>
2021-01-19 01:30:59 +0000

    Add files via upload

---
+++ javaassignment/src/main/java/com/java/assignment/mapper/EventMapper.java
@@ -0,0 +1,27 @@
+package com.java.assignment.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.java.assignment.entity.Event;
+import com.java.assignment.shared.dto.EventDto;
+
+@Component
+public class EventMapper {
+	
+	
+
+	/**
+	 * Takes eventDTO start and finish objects and calculates (non)alert event based on elapsed timestamp
+	 * 
+	 * @param startRequest
+	 * @param finishRequest
+	 * @return
+	 */
+	public Event EventDTOCalc(EventDto startRequest, EventDto finishRequest) {
+		// TODO Auto-generated method stub
+		Long duration =  finishRequest.getTimestamp() - startRequest.getTimestamp();
+        boolean isAlert = duration > 4;
+        return new Event(startRequest.getId(), duration, startRequest.getType(), startRequest.getHost(), isAlert);
+	}
+
+}
